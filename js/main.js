c6269ee36de1830e1971904d0a982c1bce3718f7
SabirIvaN <42330946+SabirIvaN@users.noreply.github.com>
2018-10-05 21:22:20 +1100

    Внесены правки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var linkMap = document.querySelector("#main__contacts-us-map");
-var formMap = document.querySelector("#modal-window-map");
-var closeMap = document.querySelector("#modal-window-map-close");
+var linkMap = document.querySelector('#main__contacts-us-map');
+var formMap = document.querySelector('#modal-window-map');
+var closeMap = document.querySelector('#modal-window-map-close');
 
 linkMap.addEventListener('click', function() {
 	event.preventDefault();
@@ -11,13 +11,13 @@ closeMap.addEventListener('click', function() {
 	formMap.classList.remove('modal-window-map-show');
 });
 
-var linkWrite = document.querySelector("#main__contacts-us-button");
-var formWrite = document.querySelector("#modal-window-write");
-var closeWrite = document.querySelector("#modal-window-write-close");
+var linkWrite = document.querySelector('#main__contacts-us-button');
+var formWrite = document.querySelector('#modal-window-write');
+var closeWrite = document.querySelector('#modal-window-write-close');
 
 linkWrite.addEventListener('click', function() {
 	event.preventDefault();
-	formWrite.classList.add("modal-window-write-show");
+	formWrite.classList.add('modal-window-write-show');
 })
 
 closeWrite.addEventListener('click', function() {
